df6c3857e34d15003e304713430f0f78ce221f4f
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-07-07 11:49:47 +0200

    primera subida

---
+++ public/js/socket-chat.js
@@ -0,0 +1,53 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has("nombre") || !params.has("sala")) {
+  window.location = "index.html";
+  throw new Error("El nombre y la sala son necesarios");
+}
+
+var usuario = {
+  nombre: params.get("nombre"),
+  sala: params.get("sala"),
+};
+
+socket.on("connect", function () {
+  console.log("Conectado al servidor");
+
+  socket.emit("entrarChat", usuario, function (resp) {
+    console.log("Usuarios conectados", resp);
+  });
+});
+
+// escuchar
+socket.on("disconnect", function () {
+  console.log("Perdimos conexión con el servidor");
+});
+
+// Enviar información
+socket.emit(
+  "enviarMensaje",
+  {
+    usuario: "Fernando",
+    mensaje: "Hola Mundo",
+  },
+  function (resp) {
+    console.log("respuesta server: ", resp);
+  }
+);
+
+// Escuchar información
+socket.on("crearMensaje", function (mensaje) {
+  console.log("Servidor:", mensaje);
+});
+
+// Escuchar información
+socket.on("listaPersonas", function (mensaje) {
+  console.log(mensaje);
+});
+
+//mensajes privados
+socket.on("mensajePrivado", function (mensaje) {
+  console.log("Mensaje privado:", mensaje);
+});
